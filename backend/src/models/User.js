90ee289639755496b5a17799f8ddf08252cf47ea
Hein Oke Soe <me@freaks.dev>
2026-02-20 11:08:00 +0700

    add User mongoose model

---
+++ backend/src/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        maxlength: [60, 'Name cannot be more than 60 characters'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please fill a valid email address',
+        ],
+    },
+    passwordHash: {
+        type: String,
+        required: [true, 'Please provide a password'],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
